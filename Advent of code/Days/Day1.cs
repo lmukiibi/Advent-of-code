50fdcb78b81d4d513638ff8c731ec9f96ce80e81
Leroy <leroy.mukiibi@gmail.com>
2020-12-12 14:22:21 +0100

    Done with day 1

--- Advent of code/Days/Day1.cs
+++ Advent of code/Days/Day1.cs
@@ -8,15 +8,30 @@ namespace Advent_of_code.Days
 {
     class Day1
     {
+        private string path = @"C:\Users\123\source\repos\Advent of code\Input codes\input day 1.txt";
         private string importedString;
+
+        List<int> listOfValues;
         public Day1()
         {
-            importedString = Import_Class.ImportFile(@"C:\Users\123\source\repos\Advent of code\Input codes");
+            importedString = Import_Class.ImportFile(path);
+            listOfValues = new List<int>();
+            listOfValues = Logics_Class.ListOfStringsToListOfInts(Logics_Class.StringToList(importedString));
         }
 
         public void Run()
         {
+            Console.WriteLine(importedString + "\n");
+            int sum = Logics_Class.FindSumOfTwoValuesOfInt(listOfValues, 2020);
+            Console.WriteLine(sum.ToString());
+            Console.WriteLine($"The sum of {listOfValues[36]} and {listOfValues[147]} is {sum}.\n" +
+                $"Multyply them and you get {listOfValues[36] * listOfValues[147]}.");
 
+            sum = Logics_Class.FindSumOfThreeValuesOfInt(listOfValues, 2020);
+            Console.WriteLine(sum.ToString());
+            Console.WriteLine($"The sum of {listOfValues[33]}, {listOfValues[69]} and {listOfValues[90]} is {sum}.\n" +
+                $"Multyply them and you get {listOfValues[33] * listOfValues[69] * listOfValues[90]}.");
+            Console.ReadKey();
         }
     }
 }
---
+++ Advent of code/Logics Class.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advent_of_code
+{
+    static class Logics_Class
+    {
+        public static List<string> StringToList(string s)
+        {
+            string buffer = "";
+            List<string> listOfStrings = new List<string>();
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i].ToString() != "\n")
+                    buffer += s[i];
+                else
+                {
+                    listOfStrings.Add(buffer);
+                    buffer = "";
+                }
+            }
+            return listOfStrings;
+
+        }
+
+        public static List<int> ListOfStringsToListOfInts(List<string> list)
+        {
+            List<int> buffer = new List<int>();
+            foreach (String s in list)
+                buffer.Add(Convert.ToInt32(s));
+            return buffer;
+        }
+
+        public static int FindSumOfTwoValuesOfInt(List<int> list, int targetSum)
+        {
+            for (int i = 0; i < list.Count; i++)
+                for (int j = 0; j < list.Count; j++)
+                    if (list[i] + list[j] == targetSum)
+                        // i = 36 - j = 147
+                        return list[i] + list[j];
+            return 0;
+
+        }
+        public static int FindSumOfThreeValuesOfInt(List<int> list, int targetSum)
+        {
+            for (int i = 0; i < list.Count; i++)
+                for (int j = 0; j < list.Count; j++)
+                    for (int k = 0; k < list.Count; k++)
+                    if (list[i] + list[j] + list[k] == targetSum)
+                        // i = 33 - j = 69 - k = 90
+                        return list[i] + list[j] + list[k];
+            return 0;
+
+        }
+    }
+}
